1aa23c70b66fa63c87249d8d9960e5578374ad33
Anya <you@example.com>
2025-01-30 19:26:43 +0700

    Изменила файл MainActivity.kt

--- app/src/main/java/com/example/delivery/MainActivity.kt
+++ app/src/main/java/com/example/delivery/MainActivity.kt
@@ -1,47 +1,19 @@
-package com.example.delivery
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.delivery.data.PostTypesRepository
 import com.example.delivery.ui.theme.DeliveryTheme
 
 class MainActivity : ComponentActivity() {
+
+    val repository = PostTypesRepository()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             DeliveryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen(repository = repository)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DeliveryTheme {
-        Greeting("Android")
     }
 }
